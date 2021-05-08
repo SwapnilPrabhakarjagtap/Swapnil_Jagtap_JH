bed91e1db3c6b051e8497c0e990575a744599fe8
SwapnilPrabhakarjagtap <83824254+SwapnilPrabhakarjagtap@users.noreply.github.com>
2021-05-08 09:44:38 +0530

    Delete ASs directory

--- ASs/Assign1.java
+++
@@ -1,9 +0,0 @@
-class Assign1
-{
-public static void main(String [] args)
-	{
-	System.out.print("Hello");
-	System.out.println("Swapnil Jagtap");
-	
-	}
-}
--- ASs/Assign9.java
+++
@@ -1,8 +0,0 @@
-class Assign9
-{
-	public static void main(String args[])
-	{
-		double d=((25.5*3.5-3.5*3.5)/(40.5-4.5));
-		System.out.println("((25.5*3.5-3.5*3.5)/(40.5-4.5))= "+d);
-	}
-}
